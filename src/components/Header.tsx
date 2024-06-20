2a45566e6873bc078b0991a65b61541adf2239b2
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-06-20 14:01:08 -0400

    feat: add header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="px-0 py-5 bg-black/80">
+      <div className="flex justify-between items-center px-5 py-0;">
+        <div className="text-white text-[25px] font-bold uppercase;">
+          <a href="/">FLIXX</a>
+        </div>
+        <nav>
+          <ul className="flex">
+            <li className="ml-5">
+              <a
+                className="text-base hover:text-[color:var(--color-secondary)]"
+                href="/"
+              >
+                Movies
+              </a>
+            </li>
+            <li className="ml-5">
+              <a
+                className="text-base hover:text-[color:var(--color-secondary)]"
+                href="/shows.html"
+              >
+                TV Shows
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
